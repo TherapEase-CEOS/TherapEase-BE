43b4b759c8c0e33ba1aaff280fe323c4e14b76f7
Sojeong Lee <sj27419@gmail.com>
2023-07-12 17:14:45 +0900

    add counselees

--- counselees/urls.py
+++ counselees/urls.py
@@ -1,9 +1,17 @@
-app_name = "counselees"
-
 from django.urls import path
-from accounts.views import UserLoginView, CounselorProfileView
+from .views import (
+    CounseleeCreateView,
+    CounseleeListView,
+    CounseleeUpdateView,
+    CounseleeCompleteView,
+    CounseleeDeleteView
+)
 
 urlpatterns = [
-    #path('login/', UserLoginView.as_view(), name='login'), #참고해서 작성하면 될 듯
-    #path('profile/<int:id>/', CounselorProfileView.as_view(), name='counselor-profile'),
+    path('create/', CounseleeCreateView.as_view(), name='counselee-create'),
+    path('list/', CounseleeListView.as_view(), name='counselee-list'),
+    path('update/<int:pk>/', CounseleeUpdateView.as_view(), name='counselee-update'),
+    path('complete/<int:pk>/', CounseleeCompleteView.as_view(), name='counselee-complete'),
+    path('delete/<int:pk>/', CounseleeDeleteView.as_view(), name='counselee-delete'),
 ]
+
