de59b3bd6c62fe6cf7a047a00a16612ecbd52e52
Sojeong Lee <sj27419@gmail.com>
2023-07-15 23:43:14 +0900

    fix dev.py

--- TherapEase-BE/settings/dev.py
+++ TherapEase-BE/settings/dev.py
@@ -2,31 +2,31 @@ from .base import *  # noqa
 
 DEBUG = True
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': env('DATABASE_NAME'),
-#        'USER': env('DATABASE_USER'),
-#        'PASSWORD': env('DATABASE_PASSWORD'),
-#        'HOST': env('DATABASE_HOST'),
-#        'PORT': env('DATABASE_PORT'),
-#        'OPTIONS': {
-#                    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#                    # 'sql_mode': 'traditional',,
-#                }
-#    }
-#}
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'therapease',
-        'USER': 'admin',
-        'PASSWORD': 'therapease',
-        'HOST': 'therapease-mysql.c81tznyca4ah.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '3306',
-        'OPTIONS':{
-            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
+       'OPTIONS': {
+                    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+                    # 'sql_mode': 'traditional',,
+                }
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'therapease',
+#        'USER': 'admin',
+#        'PASSWORD': 'therapease',
+#        'HOST': 'therapease-mysql.c81tznyca4ah.ap-northeast-2.rds.amazonaws.com',
+#        'PORT': '3306',
+#        'OPTIONS':{
+#            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
+#        }
+#    }
+#}
