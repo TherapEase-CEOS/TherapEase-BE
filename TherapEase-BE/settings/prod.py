978871fe251a5df5a198b350e0579032b7e227b1
Sojeong Lee <sj27419@gmail.com>
2023-07-15 15:39:10 +0900

    fix prod.py

--- TherapEase-BE/settings/prod.py
+++ TherapEase-BE/settings/prod.py
@@ -22,7 +22,7 @@ DATABASES = {
         'USER': 'admin',
         'PASSWORD': 'therapease',
         'HOST': 'therapease-mysql.c81tznyca4ah.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '3306',
+        'PORT': '8000',
         'OPTIONS':{
             'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
         }
