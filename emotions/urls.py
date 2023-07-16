e9b8e49ce73680a3591e55bf508dde872f7d985b
Hwang-Jaeryeong <jryeong67@naver.com>
2023-07-17 01:24:37 +0900

    [fix] urls.py

--- emotions/urls.py
+++ emotions/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import EmotionCreateView, EmotionListView
+from .views import EmotionCreateView, EmotionListView, EmotionGraphView
 
 urlpatterns = [
     path('list/<int:account_id>/', EmotionListView.as_view(), name='emotion-list'),
     path('create/<int:account_id>/', EmotionCreateView.as_view(), name='emotion-create'),
+    path('graph/<int:account_id>/', EmotionGraphView.as_view(), name='emotion-graph'),
 ]
 
