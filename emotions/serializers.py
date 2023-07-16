2ce49892d927add0c9cb84a254a698efc4abb520
Hwang-Jaeryeong <jryeong67@naver.com>
2023-07-17 01:14:11 +0900

    Save local changes

--- emotions/serializers.py
+++ emotions/serializers.py
@@ -5,5 +5,4 @@ from .models import Emotion
 class EmotionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Emotion
-        fields = ['main_emotion', 'sub_emotion', 'feeling', 'intensity', 'details1', 'details2', 'details3']
-
+        fields = ['main_emotion', 'sub_emotion', 'feeling', 'intensity', 'details1', 'details2', 'details3', 'account']
--- emotions/urls.py
+++ emotions/urls.py
@@ -1,21 +1,8 @@
-'''
 from django.urls import path
 from .views import EmotionCreateView, EmotionListView
 
 urlpatterns = [
-    path('', EmotionCreateView.as_view(), name='emotion-create'),
-    path('<int:account_id>/', EmotionListView.as_view(), name='emotion-list'),
-]
-'''
-
-from django.urls import path
-from .views import create_emotion, list_emotions, graph_emotions
-
-app_name = 'emotions'
-
-urlpatterns = [
-    path('create/<int:pk>/', create_emotion, name='create'),
-    path('list/<int:pk>/', list_emotions, name='list'),
-    path('graph/<int:pk>/', graph_emotions, name='graph'),
+    path('list/<int:account_id>/', EmotionListView.as_view(), name='emotion-list'),
+    path('create/<int:account_id>/', EmotionCreateView.as_view(), name='emotion-create'),
 ]
 
