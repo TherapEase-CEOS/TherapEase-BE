97e14b479fbc40d5f86bfcb632bea7f43af578bd
Sojeong Lee <sj27419@gmail.com>
2023-07-17 00:30:36 +0900

    del emotion urls.py

--- emotions/urls.py
+++ emotions/urls.py
@@ -1,10 +1,9 @@
-'''
 from django.urls import path
-from .views import EmotionCreateView, EmotionListView
+from .views import EmotionCreateView
 
 urlpatterns = [
     path('', EmotionCreateView.as_view(), name='emotion-create'),
-    path('<int:account_id>/', EmotionListView.as_view(), name='emotion-list'),
+    #path('<int:account_id>/', EmotionListView.as_view(), name='emotion-list'),
 ]
-'''
+
 
