ea1cf46cec46f13f49267a25e0d7dbc1c3f98b6e
Sojeong Lee <sj27419@gmail.com>
2023-07-19 22:17:21 +0900

    fix counselees url

--- counselees/urls.py
+++ counselees/urls.py
@@ -9,7 +9,7 @@ from .views import (
 #
 urlpatterns = [
     path('', CounseleeCreateView.as_view(), name='counselee-create'),
-    path('', CounseleeListView.as_view(), name='counselee-list'),
+    path('list/', CounseleeListView.as_view(), name='counselee-list'),
     path('<int:pk>/', CounseleeUpdateView.as_view(), name='counselee-update'),
     path('complete/<int:pk>/', CounseleeCompleteView.as_view(), name='counselee-complete'),
     path('delete/<int:pk>/', CounseleeDeleteView.as_view(), name='counselee-delete'),
