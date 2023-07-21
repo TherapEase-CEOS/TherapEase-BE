94a4b83b194ad637926dba09a8540e1a4aa95482
Sojeong Lee <sj27419@gmail.com>
2023-07-21 17:10:20 +0900

    fix base.py

--- TherapEase-BE/settings/base.py
+++ TherapEase-BE/settings/base.py
@@ -21,7 +21,7 @@ AUTH_USER_MODEL = 'accounts.User'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 env = environ.Env(
-    DEBUG=(bool, False)
+    DEBUG=(bool, True)
 )
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
