b6d19d854103b1d0168666dbeacef0bd8d70b546
Sojeong Lee <sj27419@gmail.com>
2023-07-22 11:40:29 +0900

    fix base.py

--- TherapEase-BE/settings/base.py
+++ TherapEase-BE/settings/base.py
@@ -33,7 +33,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', 'ec2-43-201-83-172.ap-northeast-2.compute.amazonaws.com', 'therapease.co.kr']
+ALLOWED_HOSTS = ['*']
 
 
 SIMPLE_JWT = {
