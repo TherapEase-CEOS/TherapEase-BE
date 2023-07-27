9bb07602cfe312df925f82dc8135936336ff7ff3
Sojeong Lee <sj27419@gmail.com>
2023-07-28 00:11:23 +0900

    fix git conflict error

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -9,25 +9,14 @@ class LoginSerializer(serializers.Serializer):
     code = serializers.CharField(write_only=True, required=True)
 
     def validate(self, attrs):
-<<<<<<< HEAD
-        code = attrs.get('code', None) #pw
-        name = attrs.get('name', None) #id
-=======
         #name = attrs.get('name',None)
         code = attrs.get('code', None)
->>>>>>> main
 
         try:
-            user = User.objects.get(code=code, name=name)
+            user = User.objects.get(code=code)
         except User.DoesNotExist:
             raise serializers.ValidationError("유효하지 않은 코드입니다.")
 
-<<<<<<< HEAD
-        # CustomTokenObtainPairSerializer를 사용하여 토큰 발급
-        token_serializer = CustomTokenObtainPairSerializer(data={"username": name, "password": code})
-        token_serializer.is_valid(raise_exception=True)
-        token = token_serializer.validated_data
-=======
         if user.code.startswith('ee'):
             role = 'counselee'
         elif user.code.startswith('or'):
@@ -37,7 +26,6 @@ class LoginSerializer(serializers.Serializer):
 
         refresh = RefreshToken.for_user(user)
         access = refresh.access_token
->>>>>>> main
 
         data = {
             'id': user.id,
@@ -56,6 +44,5 @@ class CounselorProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Counselor
         fields = ('name', 'contact', 'introduction')
-
 
 
