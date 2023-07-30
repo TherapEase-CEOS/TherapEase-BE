1dc036d6b41f3ea27ee8daf03806693ff25b4d2a
Hwang-Jaeryeong <jryeong67@naver.com>
2023-07-31 02:20:17 +0900

    다시?

--- schedule/views.py
+++ schedule/views.py
@@ -13,32 +13,36 @@ class ScheduleView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, pk=None):
-        if pk is None:
-            schedules = Schedule.objects.all()
+        user = request.user
+        role = user.role
+        accountId = user.accountId
 
-            # Check if any schedule exists
-            if not schedules.exists():
-                # If no schedule exists, return default values
-                default_schedule_data = {
-                    "sunday": [False] * 15,
-                    "monday": [False] * 15,
-                    "tuesday": [False] * 15,
-                    "wednesday": [False] * 15,
-                    "thursday": [False] * 15,
-                    "friday": [False] * 15,
-                    "saturday": [False] * 15,
-                }
-                return Response({'latestUpdated': None, 'data': {**default_schedule_data}})
-
-            serializer = ScheduleSerializer(schedules, many=True)
-            return Response({'data': serializer.data})
-        else:
-            try:
-                schedule = Schedule.objects.get(pk=pk)
-                serializer = ScheduleSerializer(schedule)
+        try:
+            if role == '상담사':
+                # 상담사인 경우, 자신의 스케줄을 반환합니다.
+                schedule = Schedule.objects.get(accountId=accountId)
+            else:
+                # 내담자인 경우, 연결된 상담사의 스케줄을 반환합니다.
+                counselor_schedule = Schedule.objects.get(accountId=accountId)
+                serializer = ScheduleSerializer(counselor_schedule)
                 return Response({'data': serializer.data})
-            except Schedule.DoesNotExist:
-                return Response({'message': '시간표를 찾을 수 없습니다.'}, status=404)
+
+        except Schedule.DoesNotExist:
+            # 기본 스케줄 데이터 생성
+            default_schedule_data = {
+                "sunday": [False] * 15,
+                "monday": [False] * 15,
+                "tuesday": [False] * 15,
+                "wednesday": [False] * 15,
+                "thursday": [False] * 15,
+                "friday": [False] * 15,
+                "saturday": [False] * 15,
+            }
+            # 데이터가 없는 경우 기본 스케줄 데이터를 반환합니다.
+            return Response({'latestUpdated': None, 'data': {**default_schedule_data}})
+
+        serializer = ScheduleSerializer(schedule)
+        return Response({'data': serializer.data})
 
     def put(self, request):
         try:
@@ -48,7 +52,7 @@ class ScheduleView(APIView):
 
         serializer = ScheduleSerializer(schedule, data=request.data)
         if serializer.is_valid():
-            serializer.save()
+
             response_data = {
                 'latestUpdated': date.today().strftime('%Y-%m-%d'),
                 'data': serializer.data
