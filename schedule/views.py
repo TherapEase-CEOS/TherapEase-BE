e81a7e538911cbaf11ffaff097cdfd8bd537172f
Hwang-Jaeryeong <jryeong67@naver.com>
2023-08-01 00:13:17 +0900

    date 반환형식

--- schedule/views.py
+++ schedule/views.py
@@ -31,7 +31,7 @@ class ScheduleView(APIView):
                     "friday": [False] * 15,
                     "saturday": [False] * 15,
                 }
-                return Response({'latestUpdated': None, 'data': {**default_schedule_data}})
+                return Response({'latestUpdated': formatted_datetime, 'data': {**default_schedule_data}})
 
             serializer = ScheduleSerializer(schedules, many=True)
             return Response({'latestUpdated': formatted_datetime, 'data': serializer.data})
