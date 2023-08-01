c18352b14ac02a2f69852333cfefc13bbdb30419
Sojeong Lee <sj27419@gmail.com>
2023-08-02 03:28:00 +0900

    fix counselee urls 수정

--- counselees/urls.py
+++ counselees/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('', CounseleeCreateView.as_view(), name='counselee-create'),
     path('list/', CounseleeListView.as_view(), name='counselee-list'),
     path('<int:pk>/', CounseleeUpdateView.as_view(), name='counselee-update'),
-    path('<int:pk>/', CounseleeCompleteView.as_view(), name='counselee-complete'),
-    path('<int:pk>/', CounseleeDeleteView.as_view(), name='counselee-delete'),
+    path('complete/<int:pk>/', CounseleeCompleteView.as_view(), name='counselee-complete'),
+    path('delete/<int:pk>/', CounseleeDeleteView.as_view(), name='counselee-delete'),
 ]
