28c599852a89e30731c8e96bb60a8568a34d8404
Sojeong Lee <sj27419@gmail.com>
2023-08-02 04:26:42 +0900

    fix schedule get, put

--- schedule/views.py
+++ schedule/views.py
@@ -10,10 +10,6 @@ from .models import Schedule
 from .serializers import ScheduleSerializer
 from django.db.models import F
 
-# 시간 반환형식 변경
-#current_datetime = datetime.now()
-#formatted_datetime = current_datetime.strftime('%Y-%m-%d %H:%M:%S')
-
 class IsClientUser(BasePermission):
     def has_permission(self, request, view):
         user = request.user
@@ -33,11 +29,11 @@ class ScheduleView(APIView):
         account_id = request.user.accountId # accountId를 받는다
 
         try:
-            schedule = Schedule.objects.get(id=account_id) #account_id에 해당되는 id의 상담일정표를 받음
+            schedule = Schedule.objects.get(pk=account_id) # account_id에 해당되는 id의 상담일정표를 받음
             serializer = ScheduleSerializer(schedule)
             response_data = {'data': serializer.data}
             return Response(response_data)
-        except Schedule.DoesNotExist: # 없으면 그냥 다 false인 상담일정표
+        except Schedule.DoesNotExist: # 없으면 다 false인 상담일정표 반환
             default_schedule_data = {
                 "sunday": [False] * 15,
                 "monday": [False] * 15,
@@ -52,18 +48,18 @@ class ScheduleView(APIView):
 
     def put(self, request, pk=None):
         is_client = self.is_client_user(request)
-        if is_client:
+        if is_client: # 내담자이면
             return Response({"detail": "상담사만 상담일정표를 수정할 수 있습니다."}, status=403)
+        else : # 상담사이면
+            try:
+                schedule = Schedule.objects.get(pk=pk)
+            except Schedule.DoesNotExist:
+                return Response({'message': '시간표를 찾을 수 없습니다.'}, status=404)
 
-        try:
-            schedule = Schedule.objects.get(pk=pk)
-        except Schedule.DoesNotExist:
-            return Response({'message': '시간표를 찾을 수 없습니다.'}, status=404)
-
-        serializer = ScheduleSerializer(schedule, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            response_data = {'data': serializer.data}
-            return Response(response_data)
-        else:
-            return Response(serializer.errors, status=400)
+            serializer = ScheduleSerializer(schedule, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                response_data = {'data': serializer.data}
+                return Response(response_data)
+            else:
+                return Response(serializer.errors, status=400)
