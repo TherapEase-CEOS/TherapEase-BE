cd222f938a71c43e27b3a1eefc407399e8e56048
Sojeong Lee <sj27419@gmail.com>
2023-08-02 04:05:33 +0900

    fix schedule get

--- schedule/urls.py
+++ schedule/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ScheduleView
 
 urlpatterns = [
-    path('', ScheduleView.as_view(), name='schedule'),
-    path('<int:pk>/', ScheduleView.as_view(), name='schedule-detail'),
+    #path('', ScheduleView.as_view(), name='schedule'),
+    path('<int:pk>/', ScheduleView.as_view(), name='schedule'),
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -30,36 +30,25 @@ class ScheduleView(APIView):
             return False
 
     def get(self, request, pk=None):
-        if pk is None:
-            schedules = Schedule.objects.all()
+        account_id = request.user.accountId # accountId를 받는다
 
-            # 시간표가 없을 때 기본 값을 반환합니다.
-            if not schedules.exists():
-                default_schedule_data = {
-                    "sunday": [False] * 15,
-                    "monday": [False] * 15,
-                    "tuesday": [False] * 15,
-                    "wednesday": [False] * 15,
-                    "thursday": [False] * 15,
-                    "friday": [False] * 15,
-                    "saturday": [False] * 15,
-                }
-#                response_data = {'latestUpdated': formatted_datetime, 'data': [default_schedule_data]}
-                response_data = {'data': [default_schedule_data]}
-
-                return Response(response_data)
-
-            serializer = ScheduleSerializer(schedules, many=True)
+        try:
+            schedule = Schedule.objects.get(id=account_id) #account_id에 해당되는 id의 상담일정표를 받음
+            serializer = ScheduleSerializer(schedule)
             response_data = {'data': serializer.data}
             return Response(response_data)
-        else:
-            try:
-                schedule = Schedule.objects.get(pk=pk)
-                serializer = ScheduleSerializer(schedule)
-                response_data = {'data': serializer.data}
-                return Response(response_data)
-            except Schedule.DoesNotExist:
-                return Response({'message': '시간표를 찾을 수 없습니다.'}, status=404)
+        except Schedule.DoesNotExist: # 없으면 그냥 다 false인 상담일정표
+            default_schedule_data = {
+                "sunday": [False] * 15,
+                "monday": [False] * 15,
+                "tuesday": [False] * 15,
+                "wednesday": [False] * 15,
+                "thursday": [False] * 15,
+                "friday": [False] * 15,
+                "saturday": [False] * 15,
+            }
+            response_data = {'data': [default_schedule_data]}
+            return Response(response_data)
 
     def put(self, request, pk=None):
         is_client = self.is_client_user(request)
