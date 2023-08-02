594f47e43783e43bc2b1da70c257d430ba345a5d
Hwang-Jaeryeong <jryeong67@naver.com>
2023-08-02 14:41:22 +0900

    [fix] profile url

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from .views import UserLoginView, CounselorProfileView, check_token, LogoutView
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
-    path('profile/<int:pk>/', CounselorProfileView.as_view(), name='counselor-profile'),
+    path('profile/', CounselorProfileView.as_view(), name='counselor-profile'),
     path('check/', check_token, name='check-token'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
